6792b583e942fd8ebe8672125e171092828401eb
altkzs <dogmagoa@gmail.com>
2024-12-10 12:07:54 +0900

    fix:인벤토리 끝

--- Assets/Scripts/UI/ItemSlot.cs
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -1,24 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
 	public ItemData item;
+	public Button button;
+	public Image icon;
+	public TextMeshProUGUI quantityText;
+	private Outline outline;
 	public UIInventoty inventoty;
 
 	public int index;
 	public bool equipped;
 	public int quantity;
-	// Start is called before the first frame update
-	void Start()
-	{
 
+
+	private void Awake()
+	{
+		outline = GetComponent<Outline>();
 	}
-
-	// Update is called once per frame
-	void Update()
+	public void OnEnable()
 	{
+		outline.enabled = equipped;
+	}
+	public void Set()
+	{
+		icon.gameObject.SetActive(true);
+		icon.sprite = item.icon;
+		quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
 
+		if (outline != null)
+		{
+			outline.enabled = equipped;
+		}
+	}
+	public void Clear()
+	{
+		item = null;
+		icon.gameObject.SetActive(false);
+		quantityText.text = string.Empty;
+	}
+	public void OnClickButton()
+	{
+		inventoty.SelectItem(index);
 	}
 }
--- Assets/Scripts/UI/UIInventoty.cs
+++ Assets/Scripts/UI/UIInventoty.cs
@@ -6,6 +6,7 @@ public class UIInventoty : MonoBehaviour
 	public ItemSlot[] slots;
 	public GameObject inventotyWindow;
 	public Transform slotPanel;
+	public Transform dropPosition;
 
 	[Header("Select Item")]
 	public TextMeshProUGUI selectedItemName;
@@ -21,11 +22,14 @@ public class UIInventoty : MonoBehaviour
 	private PlayerController controller;
 	private PlayerCondition condition;
 
+	ItemData selectedItem;
+	int selectedItemIndex = 0;
 
 	void Start()
 	{
 		controller = CharacterManager.Instance.player.Controller;
 		condition = CharacterManager.Instance.player.condition;
+		dropPosition = CharacterManager.Instance.player.dropPosition;
 
 		controller.inventory += Toggle;
 		CharacterManager.Instance.player.addItem += AddItem;
@@ -110,28 +114,100 @@ public class UIInventoty : MonoBehaviour
 	}
 	void UpdateUI()
 	{
-
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (slots[i].item != null)
+			{
+				slots[i].Set();
+			}
+			else
+			{
+				slots[i].Clear();
+			}
+		}
 	}
-
 
 	ItemSlot GetItemStack(ItemData data)
 	{
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (slots[i].item == data && slots[i].quantity < data.maxStackAmount)
+			{
+				return slots[i];
+			}
+		}
 		return null;
 	}
 	ItemSlot GetEmptySlot()
 	{
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (slots[i].item == null)
+			{
+				return slots[i];
+			}
+		}
 		return null;
 	}
 	void ThrowItem(ItemData data)
 	{
-
+		Instantiate(data.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));
 	}
-	public void Set()
+	public void SelectItem(int index)
 	{
+		if (slots[index].item == null) return;
+		selectedItem = slots[index].item;
+		selectedItemIndex = index;
 
+		selectedItemName.text = selectedItem.displayName;
+		selectedItemDescription.text = selectedItem.description;
+		selectedStatName.text = string.Empty;
+		selectedStatValue.text = string.Empty;
+		for (int i = 0; i < selectedItem.consumables.Length; i++)
+		{
+			selectedStatName.text += selectedItem.consumables[i].type.ToString() + "\n";
+			selectedStatValue.text += selectedItem.consumables[i].value.ToString() + "\n";
+		}
+
+		useButton.SetActive(selectedItem.type == ItemType.Consumable);
+		equipButton.SetActive(selectedItem.type == ItemType.Equipable && !slots[index].equipped);
+		unEquipButton.SetActive(selectedItem.type == ItemType.Equipable && !slots[index].equipped);
+		dropButton.SetActive(true);
 	}
-	public void Clear()
+	public void OnUseButton()
 	{
-
+		if (selectedItem.type == ItemType.Consumable)
+		{
+			for (int i = 0; i < selectedItem.consumables.Length; i++)
+			{
+				switch (selectedItem.consumables[i].type)
+				{
+					case ConsumableType.Health:
+						condition.Heal(selectedItem.consumables[i].value);
+						break;
+					case ConsumableType.Huger:
+						condition.Eat(selectedItem.consumables[i].value);
+						break;
+				}
+			}
+			RemoveSelctedItem();
+		}
+	}
+	public void OnDropButton()
+	{
+		ThrowItem(selectedItem);
+		RemoveSelctedItem();
+	}
+	void RemoveSelctedItem()
+	{
+		slots[selectedItemIndex].quantity--;
+		if (slots[selectedItemIndex].quantity <= 0)
+		{
+			selectedItem = null;
+			slots[selectedItemIndex].item = null;
+			selectedItemIndex = -1;
+			ClearSelectedItemWindow();
+		}
+		UpdateUI();
 	}
 }
